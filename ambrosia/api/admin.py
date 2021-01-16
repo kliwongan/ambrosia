d72eacb7213509daae77d42b8ce4c505bd9d2ad2
kliwongan <65840274+kliwongan@users.noreply.github.com>
2021-01-16 12:08:41 -0700

    Delete ambrosia/api directory
    Fixed app directory level

--- ambrosia/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Ingredient, Food_Storage, Storageingredient, Meal, Mealingredient
-# Register your models here.
-
-admin.site.register(Ingredient)
-admin.site.register(Food_Storage)
-admin.site.register(Storageingredient)
-admin.site.register(Meal)
-admin.site.register(Mealingredient)
