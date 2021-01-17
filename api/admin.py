2457a0b78636a8f23f68a2d5bf5bd3c9727de75d
Jocn2020 <63906312+Jocn2020@users.noreply.github.com>
2021-01-17 19:24:07 +0700

    Add more models to admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Ingredient, Food_Storage, Storageingredient, Meal, Mealingredient
+from .models import Ingredient, Food_Storage, Storageingredient, Meal, Mealingredient, Daily_Schedule, User_Schedule
 # Register your models here.
 
 admin.site.register(Ingredient)
@@ -7,3 +7,5 @@ admin.site.register(Food_Storage)
 admin.site.register(Storageingredient)
 admin.site.register(Meal)
 admin.site.register(Mealingredient)
+admin.site.register(Daily_Schedule)
+admin.site.register(User_Schedule)
