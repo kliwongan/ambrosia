d53cc66ca970c05dfea6ba95ecc6ab55f368bd40
Jocn2020 <63906312+Jocn2020@users.noreply.github.com>
2021-01-17 19:29:59 +0700

    Add more functions
    Added functions:
    add meals to schedule
    separate available meal and unavailable meal
    update subingredient so it can find number of ingredients needed left

--- api/views.py
+++ api/views.py
@@ -1,14 +1,14 @@
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Ingredient, Food_Storage, Storageingredient, User, Meal, Mealingredient, User_Schedule, Daily_Schedule
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import IngredientSerializer, Food_StorageSerializer, MealSerializer, StorageIngredientSerializer
-from .models import Ingredient, Food_Storage, Storageingredient, User, Meal, Mealingredient
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
-from django.db import IntegrityError
-
+from .serializers import IngredientSerializer, Food_StorageSerializer, MealSerializer, UserScheduleSerializer, CreateScheduleSerializer
+from .forms import StorageForm
 # Create your views here.
 
 
@@ -28,8 +28,38 @@ class MealView(generics.ListAPIView):
     serializer_class = MealSerializer
 
 
+class UserScheduleView(generics.ListAPIView):
+    queryset = User_Schedule.objects.all()
+    serializer_class = UserScheduleSerializer
+
+
+class CreateScheduleView(APIView):
+    serializer_class = CreateScheduleSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = request.user.username
+            meal = serializer.data.get('meal')
+            date = serializer.data.get('date')
+            new_meal = Meal.objects.get(name=meal)
+            new_schedule = Daily_Schedule(date=date, meal=new_meal)
+            new_schedule.save()
+            user_schedule = User_Schedule(
+                user=user, schedule=new_schedule)
+            user_schedule.save()
+
+        return Response(UserScheduleSerializer(user_schedule).data, status=status.HTTP_201_CREATED)
+
+
 def index(request):
-    return render(request, 'app/index.html')
+    if request.user.is_authenticated:
+        return Meal_recommendation(request)
+    else:
+        return login_view(request)
 
 
 def login_view(request):
@@ -80,6 +110,8 @@ def register(request):
             })
         new_storage = Food_Storage(user=username)
         new_storage.save()
+        new_schedule = User_Schedule(user=username)
+        new_schedule.save()
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
@@ -90,7 +122,7 @@ def addingredient(request):
     if request.method == 'POST':
         user = request.POST.get('name', False)
         # user = request.user.username
-        quantity = int(request.POST.get('quantity', False))
+
         ingredients = request.POST.get(
             'ingredients', False)  # expect a dictionary
         storage = Food_Storage.objects.filter(user=user)
@@ -100,35 +132,33 @@ def addingredient(request):
         storage = Food_Storage.objects.get(user=user)
 
         # the code when ingredients is in form of dictionary
-        """ for ingredient in ingredients.keys():
+        # if the input is dictionary
+
+        ''' for ingredient in ingredients.keys():
             check_ingredient = Ingredient.objects.filter(name=ingredient)
             if check_ingredient.count() == 0:
-                new_ingredient = Ingredient(name=ingredient)
-                new_ingredient.save()
-            check_ingredient = Ingredient.objects.get(name=ingredient)
-            if add_items.count() == 0:
-                add_items = Storageingredient(
-                    ingredient=check_ingredient, storage=storage, quantity=quantity)
+                check_ingredient = Ingredient(name=ingredient)
+                check_ingredient.save()
             else:
-                add_items = add_items[0]
-                add_items.quantity += quantity
-            add_items.save()"""
+                check_ingredient = Ingredient.objects.get(name=ingredient)
+            add_items = Storageingredient.objects.filter(
+                ingredient=check_ingredient, storage=storage)
+            # add inputed ingredients
+            add_items.quantity += ingredients[ingredient] '''
+
+        # if input is not dictionary, we have quantity as integer variable
+        # if quantity is an integer
+        quantity = int(request.POST.get('quantity', False))
 
         check_ingredient = Ingredient.objects.filter(name=ingredients)
         if check_ingredient.count() == 0:
             new_ingredient = Ingredient(name=ingredients)
             new_ingredient.save()
         check_ingredient = Ingredient.objects.get(name=ingredients)
-
         add_items = Storageingredient.objects.filter(
             ingredient=check_ingredient, storage=storage)
-        if add_items.count() == 0:
-            add_items = Storageingredient(
-                ingredient=check_ingredient, storage=storage, quantity=quantity)
-        else:
-            add_items = add_items[0]
-            add_items.quantity += quantity
-        add_items.save()
+        # add inputed ingredients
+        add_items.quantity += quantity
 
         storage_items = Storageingredient.objects.filter(
             storage=storage).all()
@@ -139,10 +169,10 @@ def addingredient(request):
         return HttpResponseRedirect(reverse('index'))
 
 
-# storage: object, ingredients: dictionary/string, #quantity: dictionary
+# storage: object, ingredients: dictionary/string, #quantity: dictionary or variable
 def enough_ingredient(storage, ingredients, quantity):
     sub_possible = True
-    check_ingredient = Ingredient.objects.filter(name=ingredients)
+    """check_ingredient = Ingredient.objects.filter(name=ingredients)
     if check_ingredient.count() == 0:
         sub_possible = False
     else:
@@ -154,23 +184,23 @@ def enough_ingredient(storage, ingredients, quantity):
         else:
             add_items = add_items[0]
             if add_items.quantity < quantity:
-                sub_possible = False
+                sub_possible = False"""
 
-    # for multiple input
-    """ for ingredient in ingredients.keys(): 
-           check_ingredient = Ingredient.objects.filter(name=ingredient)
-            if check_ingredient.count() == 0:
+    # for multiple input, quantity is in dictionary
+    for ingredient in ingredients:
+        check_ingredient = Ingredient.objects.filter(name=ingredient)
+        if check_ingredient.count() == 0:
+            sub_possible = False
+        else:
+            check_ingredient = Ingredient.objects.get(name=ingredient)
+            add_items = Storageingredient.objects.filter(
+                ingredient=check_ingredient, storage=storage)
+            if add_items.count() == 0:
                 sub_possible = False
             else:
-                check_ingredient = Ingredient.objects.get(name=ingredient)
-                add_items = Storageingredient.objects.filter(
-                    ingredient=check_ingredient, storage=storage)
-                if add_items.count() == 0:
+                add_items = add_items[0]
+                if add_items.quantity < quantity[ingredient.name]:
                     sub_possible = False
-                else:
-                    add_items = add_items[0]
-                    if add_items.quantity < quantity[ingredient]:
-                        sub_possible = False """
 
     return sub_possible
 
@@ -179,7 +209,10 @@ def subingredient(request):
     if request.method == 'POST':
         user = request.POST.get('name', False)
         # user = request.user.username
+
+        # if the input is integer
         quantity = int(request.POST.get('quantity', False))
+
         sub_possible = True
         ingredients = request.POST.get(
             'ingredients', False)  # expect a dictionary
@@ -200,20 +233,36 @@ def subingredient(request):
                 add_items = add_items[0]
                 if add_items.quantity < quantity:
                     sub_possible = False
+                    ingredient_needed[ingredients] = quantity - \
+                        add_items.quantity
 
         # the code when ingredients is in form of dictionary
-        """ for 
-                ingredient_needed[ingredient] = quantity - add_items.quantity"""
+        """ for ingredient in ingredients:
+            check_ingredient = Ingredient.objects.filter(name=ingredient)
+            if check_ingredient.count() == 0:
+                sub_possible = False
+                ingredient_needed[ingredients] = ingredients[ingredient]
+            else:
+                check_ingredient = Ingredient.objects.get(name=ingredient)
+                add_items = Storageingredient.objects.filter(
+                    ingredient=check_ingredient, storage=storage)
+                if add_items.count() == 0:
+                    sub_possible = False
+                else:
+                    add_items = add_items[0]
+                    if add_items.quantity < ingredients[ingredient]:
+                        sub_possible = False
+                        ingredient_needed[ingredient] = ingredients[ingredient] - add_items.quantity"""
 
         message = ''
-        sub_possible = enough_ingredient(storage, ingredients, quantity)
+        # sub_possible = enough(storage, ingredients.keys(), ingredients) #when ingreidents is a dictionary
         if sub_possible == True:
-            add_items = Storageingredient.objects.get(
+            sub_items = Storageingredient.objects.get(
                 ingredient=check_ingredient, storage=storage)
-            add_items.quantity -= quantity
-            add_items.save()
+            sub_items.quantity -= quantity
+            sub_items.save()
         else:
-            message = 'Your ingridients is not enough'
+            message = 'Your ingredients is not enough'
 
         storage_items = Storageingredient.objects.filter(
             storage=storage).all()
@@ -234,3 +283,63 @@ def Storage(request):
         return render(request, 'app/index.html', {
             'Storage': storage_items
         })
+
+
+def Meal_recommendation(request):
+    user = request.user.username
+    storage = Food_Storage.objects.get(user=user)
+    schedules = User_Schedule.objects.get(user=user).schedule.all()
+    meals = Meal.objects.all()
+    available_meal = []
+    non_available_meal = []
+    for meal in meals:
+        ingredients = meal.recipe.all()
+        ingredient_recipe = {}
+        for ingredient in ingredients:
+            ingredient_obj = Mealingredient.objects.get(
+                ingredient=ingredient, meal=meal)
+            ingredient_recipe[ingredient.name] = ingredient_obj.quantity
+        can_be_made = enough_ingredient(
+            storage, ingredients, ingredient_recipe)
+        if can_be_made:
+            available_meal.append(meal)
+        else:
+            non_available_meal.append(meal)
+    return render(request, 'app/schedule.html', {
+        'schedules': schedules,
+        'available_meal': available_meal,
+        'non_available_meal': non_available_meal
+    })
+
+
+def add_meal(request):
+    if request.method == 'POST':
+        meal = request.POST.get('meal', False)
+        date = request.POST.get('date', False)
+        storage = Food_Storage.objects.get(user=request.user.username)
+        add_meal = Meal.objects.get(name=meal)
+        meal_ingredients = add_meal.recipe.all()
+        for ingredient in meal_ingredients:
+            user_ingredient = Storageingredient.objects.get(
+                storage=storage, ingredient=ingredient)
+            meal_recipe = Mealingredient.objects.get(
+                meal=add_meal, ingredient=ingredient)
+            user_ingredient.quantity -= meal_recipe.quantity
+            user_ingredient.save()
+
+        add_schedule = Daily_Schedule(meal=add_meal)
+        add_schedule.date = date
+        add_schedule.save()
+        new_schedule = User_Schedule.objects.get(user=request.user.username)
+        new_schedule.schedule.add(add_schedule)
+        new_schedule.save()
+
+        return Meal_recommendation(request)
+    else:
+        return Meal_recommendation(request)
+
+
+def checklist(request):
+    return render(request, 'app/checklist.html', {
+        'form': StorageForm()
+    })
