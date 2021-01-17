76b624ce7d9511ad0ab3e86454781a389127678f
Jocn2020 <63906312+Jocn2020@users.noreply.github.com>
2021-01-17 19:24:58 +0700

    Add daily_schedule and User_schedule models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
+import datetime
 
 
 class User(AbstractUser):
@@ -9,7 +10,7 @@ class User(AbstractUser):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=64)
-    special_item = models.CharField(max_length=64, default="No")
+    eatable_food = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.name}'
@@ -39,11 +40,34 @@ class Meal(models.Model):
     name = models.CharField(max_length=64)
     recipe = models.ManyToManyField(
         Ingredient, default=None, related_name='recipes', through='Mealingredient')
-    Cooking_time = models.TimeField(default=None)
+    cooking_time = models.IntegerField(default=0)
+    description = models.TextField(default='')
+
+    def __str__(self):
+        return f'Meal: {self.name}'
 
 
 class Mealingredient(models.Model):
     meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(
-        Ingredient, on_delete=models.CASCADE)
+        Ingredient, on_delete=models.CASCADE, related_name='quantity2')
     quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.meal} need {self.quantity}x {self.ingredient}'
+
+
+class Daily_Schedule(models.Model):
+    date = models.DateField(default=datetime.datetime.now)
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return f'{self.meal.name} scheduled for {self.date}'
+
+
+class User_Schedule(models.Model):
+    user = models.CharField(max_length=64)
+    schedule = models.ManyToManyField(Daily_Schedule, default=None)
+
+    def __str__(self):
+        return f'user {self.user} schedule'
